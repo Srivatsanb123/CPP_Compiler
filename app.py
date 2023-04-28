b5bcea026d93e01dd95743fdd544f695a5cbcb77
Srivatsanb123 <srivatsanb123@gmail.com>
2023-04-28 22:30:05 +0530

    Modified

--- app.py
+++ app.py
@@ -2,16 +2,25 @@ from flask import Flask, render_template, request
 import subprocess
 import os
 import time
+import sqlite3
+
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     output = ''
+    history = []
+    code=request.form.get('cppcode')
+    conn=sqlite3.connect('cppcodes.db')
+    c=conn.cursor()
+    c.execute('CREATE TABLE IF NOT EXISTS history (id INTEGER PRIMARY KEY AUTOINCREMENT, code TEXT)')
+    sql_query = "INSERT INTO history (code) VALUES (?)"
+    c.execute(sql_query, (code,))
+    conn.commit()
     if request.method == 'POST':
-        code=request.form.get('cppcode')
-        with open('cppcode.cpp','w') as f:
-            f.write(code)
+        while not os.path.exists('cppcode.cpp'):
+            time.sleep(0.1)
         try:
             subprocess.run(['g++', 'cppcode.cpp', '-o', 'cppcode'],  stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
         except subprocess.CalledProcessError as error:
@@ -25,7 +34,25 @@ def index():
         output = out + err
         os.remove('cppcode.cpp')
         os.remove('cppcode.exe')
-    return render_template('index.html', output=output)
+    c.execute('SELECT * FROM history WHERE code IS NOT NULL')
+    rows = c.fetchall()
+    for row in rows:
+        history.append(row)
+    conn.close()
+    return render_template('index.html', output=output, history=history, cppcode=code)
+
+
+@app.route('/history', methods=['GET', 'POST'])
+def history():
+    history = []
+    conn=sqlite3.connect('cppcodes.db')
+    c=conn.cursor()
+    c.execute('SELECT * FROM history WHERE code IS NOT NULL')
+    rows = c.fetchall()
+    for row in rows:
+        history.append(row)
+    conn.close()
+    return render_template('history.html',history=history)
 
 if __name__ == '__main__':
     app.run(debug=True)
