e8262ba6fee9373ff9d8f5b9989e53f35eab795c
Srivatsanb123 <srivatsanb123@gmail.com>
2023-05-28 11:39:39 +0530

    C++ Compiler

--- static/js/Logout.js
+++ static/js/Logout.js
@@ -1,10 +1,11 @@
 var logoutBtn = document.querySelector('.Logout');
 
-logoutBtn.addEventListener('click', function() {
+logoutBtn.addEventListener('click', function(event) {
+  event.preventDefault();
   var confirmLogout = confirm('Are you sure you want to logout?');
   
 if (confirmLogout) {
-    window.location.href = './login';    
+    window.location.href = './logout';    
     console.log('User has logged out.');
   }
 });
